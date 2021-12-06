ee4bf25521165bfea4bb9482b3921faea8cd9506
JosielMatos <josielmatos77@protonmail.com>
2021-12-06 10:45:06 -0300

    Add global styles setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-import styles from '../styles/Home.module.scss';
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello, Friend!</h1>
-  )
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello, Friend!</h1>
+    </>
+  );
 }
