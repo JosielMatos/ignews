dd3b825f4f9eb6a4fc4d7c8f5245472f39683321
JosielMatos <josielmatos77@protonmail.com>
2021-12-12 11:58:59 -0300

    Fetch and format stripe data server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import avatar from "../../public/images/avatar.svg"
+
 import { SubscribeButton } from "../components/SubscribeButton";
 
-import styles from './home.module.scss';
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+import styles from "./home.module.scss";
+import avatar from "../../public/images/avatar.svg";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,17 +26,37 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👋 Hey, welcome!</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
-        <Image src={avatar} alt="Girl coding"/>
+        <Image src={avatar} alt='Girl coding' />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve(process.env.STRIPE_PRICE_KEY);
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
