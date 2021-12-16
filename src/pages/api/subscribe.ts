e3d4fa4f4251793e719178557fa746d70e2c0c9b
JosielMatos <josielmatos77@protonmail.com>
2021-12-16 16:09:32 -0300

    Fix creation of duplicated customers

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,21 +1,50 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { query as q } from "faunadb";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async function Subscribe(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await getSession({ req });
-
-  const stripeCustomer = await stripe.customers.create({
-    email: session.user.email,
-  });
-
   if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
+
     // Stripe checkout
     const checkoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: process.env.STRIPE_PRICE_KEY, quantity: 1 }],
@@ -26,6 +55,7 @@ export default async function Subscribe(
     });
 
     return res.status(200).json({ sessionId: checkoutSession.id });
+    
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not Allowed");
