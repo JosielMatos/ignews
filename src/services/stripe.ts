41ced76bcbe01ccbf0641b5d0934cdd9af37c3d5
JosielMatos <josielmatos77@protonmail.com>
2021-12-16 15:39:17 -0300

    Add stripe-js create customer

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
-    name: "New Business",
+    name: "business-insider",
     version: "1.0",
   },
 });
