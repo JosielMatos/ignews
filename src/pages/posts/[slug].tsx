71e3e4fd2ad26b6e279dbb97d6afeaab6e944293
JosielMatos <josielmatos77@protonmail.com>
2021-12-22 15:48:00 -0300

    Add prevent access without subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/react";
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
 
@@ -44,8 +44,13 @@ export const getServerSideProps: GetServerSideProps = async ({
   const session = await getSession({ req });
   const { slug } = params;
 
-  if (!session) {
-    //TODO
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
   }
 
   const prismic = getPrismicClient(req);
